5688dec0cea5aecf9c4e846b09d4fc17551f5bcb
George Olson and Sky Rousse <sky@>
2016-06-01 09:23:31 -0700

    Add letter content, form to enter name, and script to add name to letter that is revealed on submit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+  var nameInput = $("input#name").val();
+  $(".name").text(nameInput);
+  $("#letter").show();
+  event.preventDefault();
+  });
+});
